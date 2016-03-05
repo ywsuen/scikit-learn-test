93c67576690eb2be7a8eb1ab5d1b425528b657b0
ywsuen <yw_suen@outlook.com>
2016-03-05 18:21:07 +0800

    Added files via upload

--- test.py
+++ test.py
@@ -13,9 +13,16 @@ from sklearn import datasets
 import numpy as np
 #import cPickle as pickle
 
-#import adultDataset2array
+import adultDataset2array
 
-#算法核心
+# 朴素贝叶斯
+def naive_bayes_classifier(trainX, trainY):
+    from sklearn.naive_bayes import MultinomialNB
+    model = MultinomialNB()
+    model.fit(trainX, trainY)
+    return model
+
+#svm
 def svm_classifier(trainX, trainY):
     from sklearn.svm import SVC
     model = SVC(gamma=0.001, C=100.)
@@ -28,8 +35,8 @@ if __name__ == '__main__':
     data_file=""
     tresh = 0.5
     
-    test_classifiers = ['SVM']
-    classifiers = {#'NB':naive_bayes_classifier,
+    test_classifiers = ['朴素贝叶斯','SVM']
+    classifiers = {'朴素贝叶斯':naive_bayes_classifier,
                   #'KNN':knn_classifier,
                    #'LR':logistic_regression_classifier,
                   #'RF':random_forest_classifier,
@@ -45,10 +52,10 @@ if __name__ == '__main__':
     trainY = digits.target[:1200]
     testX = digits.data[1200:]
     testY = digits.target[1200:]
-    print("****************************数据预览****************************")
-    print("\n训练的X\n", trainX, "\n训练的Y\n", trainY, "\n测试的X\n",\
-    testX, "\n测试的Y\n", testY)
-    print("**************************数据预览完毕***************************")
+    #print("****************************数据预览****************************")
+    #print("\n训练的X\n", trainX, "\n训练的Y\n", trainY, "\n测试的X\n",\
+    #testX, "\n测试的Y\n", testY)
+    #print("**************************数据预览完毕***************************")
     
     for key in classifiers:#获得字典key
         print("---使用%s---" % key)
